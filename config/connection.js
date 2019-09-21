3830b92ee937be00e4ac73a85b867c45c44c7417
BrianKohnen <briankohnen@yahoo.com>
2019-09-21 14:17:17 -0500

    bk--heroku

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ const mysql = require("mysql");
 let connection = mysql.createConnection({
   host: "tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "	b7zpug571jc36pk1",
-  password: "	oa6ppm563466f0l4",
+  user: "b7zpug571jc36pk1",
+  password: "oa6ppm563466f0l4",
   database: "oal33gyrlbx5t1ov"
 });
 
